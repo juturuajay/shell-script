01045bee5b9c15f187b812a516e662a487c6b45d
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-04 11:40:20 +0530

    run time variables code

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 #these 1,2 are run time variables
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing"
+echo "$PERSON2:: I'm doing well"
