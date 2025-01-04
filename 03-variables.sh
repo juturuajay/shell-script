3c365cf739f92689d69979ef7a9f108a1460595d
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-04 11:22:34 +0530

    variable file

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh #no space before and after =
+PERSON2=suresh
+
+echo "Rahim:: Hi Robert"
+echo "Robert:: Hello Rahim"
+echo "Rahim:: How are you doing"
+echo "Robert:: I'm doing well"
