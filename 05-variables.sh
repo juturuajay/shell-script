62a5957b34af00eae645f2036579a4d95ab02015
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-04 11:57:33 +0530

    run time variables code

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,10 @@
 
 echo "please enter your username::" #text entered here will be added as value to variable
 
-read USERNAME # Here USERNAME is the variable name
+read USERNAME # Here USERNAME is the variable name  
 
 echo "username entered: $USERNAME"
+
+echo "Please enter your password:"
+
+read -s PASSWORD #-s is used for hidden the password
