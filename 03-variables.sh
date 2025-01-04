6857fae9ce1613b37761bf8c9075a622fb7b503b
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-04 11:26:33 +0530

    variables updated

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh #no space before and after =
 PERSON2=suresh
 
-echo "Rahim:: Hi Robert"
-echo "Robert:: Hello Rahim"
-echo "Rahim:: How are you doing"
-echo "Robert:: I'm doing well"
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing"
+echo "$PERSON2:: I'm doing well"
