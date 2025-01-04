cf55ee625e5db304794f03b7cbc6fe6149285f6c
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-04 23:31:48 +0530

    install mysql

--- 10-install mysql.sh
+++ 10-install mysql.sh
@@ -2,4 +2,38 @@
 
 USERID=$(id -u)
 
-dnf install mysql -y
+if [ $USERID -ne 0 ]
+  then
+     echo "ERROR: you must have sudo access to excute this script"
+     exit 1 #other than 0 we have to give here to exit the script from here
+fi
+
+dnf list installed mysql 
+ if [ $? -ne 0 ]
+ then 
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+   then 
+      echo "installing mysql....failure"
+      exit 1
+    else 
+      echo "installing mysql...success"
+    fi
+else 
+  echo "mysql is already...installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then                    #not installed
+   dnf install git -y
+   if [ $? -ne 0 ]
+   then
+      echo "git is installing...failure"
+      exit 1
+    else
+       echo "git is installing...success"
+    fi
+else
+  echo " git is already...installed"
+fi
