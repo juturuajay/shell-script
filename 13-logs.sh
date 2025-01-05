782eded080d71d9f97c1c3a68565c7a90b584100
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-05 12:53:07 +0530

    colour codes

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,8 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
+
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -12,10 +14,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE(){
   if [ $1 -ne 0 ]
    then 
-      echo -e "$2....$R failure"
+      echo -e "$2....$R failure $N"
       exit 1
     else 
-      echo -e "$2... $G success"
+      echo -e "$2... $G success $N"
     fi
 }
 
@@ -33,15 +35,15 @@ dnf list installed mysql &>>$LOG_FILE_NAME
    dnf install mysql -y &>>$LOG_FILE_NAME
    VALIDATE $? "Installing mysql"
  else 
-   echo -e "mysql is already... $Y installed"
+   echo -e "mysql is already... $Y installed $N"
 fi
 
 dnf list installed git &>>$LOG_FILE_NAME
  if [ $? -ne 0 ]
  then                    #not installed
    dnf install git -y &>>$LOG_FILE_NAME
-   VALIDATE $? "Installing git"
+   VALIDATE $? "Installing git $N"
    
  else
-   echo -e "git is already... $Y installed"
+   echo -e "git is already... $Y installed $N"
  fi
