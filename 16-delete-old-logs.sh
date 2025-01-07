f05356e5a2c6f528638972e37ae46665943958c7
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-07 22:58:29 +0530

    old logs

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -25,5 +25,5 @@ VALIDATE(){
 
 echo "script started exceuting.. $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES_TO_DELETE=$(find $SOURCE_DIR "*.log" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
