754a7e0a2b19c217aa5708129d6344d32aa6c08e
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-07 23:00:09 +0530

    old logs

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -8,22 +8,22 @@ N="\e[0m"
 
 SOURCE_DIR="/home/ec2-user/app-logs"  #from this path needs to dlt files
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+# LOGS_FOLDER="/var/log/shellscript-logs"
+# LOG_FILE=$(echo $0 | cut -d "." -f1)
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-  if [ $1 -ne 0 ]
-   then 
-      echo -e "$2....$R failure $N"
-      exit 1
-    else 
-      echo -e "$2... $G success $N"
-    fi
-}
+# VALIDATE(){
+#   if [ $1 -ne 0 ]
+#    then 
+#       echo -e "$2....$R failure $N"
+#       exit 1
+#     else 
+#       echo -e "$2... $G success $N"
+#     fi
+# }
 
 echo "script started exceuting.. $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "Files to be deleted: $FILES_TO_DELETE"
+echo "Files to be deleted: $FILES_TO_DELETE \n"
