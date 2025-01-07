9fbd58173e47e237be14327a0acc0f45641925a7
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-07 23:17:52 +0530

    old logs

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -25,7 +25,7 @@ SOURCE_DIR="/home/ec2-user/app-logs"  #from this path needs to dlt files
 
 # echo "script started exceuting.. $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.java" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r file
