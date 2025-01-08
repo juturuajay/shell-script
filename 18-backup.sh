0f4e6871b7958402c9a7a2e87ce54edf2b36c19d
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-08 10:36:00 +0530

    backup logs

--- 18-backup.sh
+++ 18-backup.sh
@@ -54,11 +54,11 @@ echo "script started exceuting.. $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-echo "Files are : $FILES"
-
-if [ -n "$FILES" ]
+if [ -n "$FILES" ] #true if there are files to zip
 then
   echo "Files are: $FILES"
+  ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+  find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip @ "$ZIP_FILE"
 else 
   echo "No Files to zip"
 fi
