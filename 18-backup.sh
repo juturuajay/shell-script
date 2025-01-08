646f0e8da272aa430ac7b3a17a24577494d13e1d
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-08 10:03:37 +0530

    backup logs

--- 18-backup.sh
+++ 18-backup.sh
@@ -27,14 +27,15 @@ VALIDATE(){
 }
 
 USAGE(){
-    echo -e "$R USAGE: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e " $R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }  
 
-mkdir -p "/home/ec2-user/shellscript-logs"
+mkdir -p /home/ec2-user/shellscript-logs
+
 if [ $# -lt 2 ]
 then 
-  echo USAGE
+  USAGE
 fi
 
 echo "script started exceuting.. $TIMESTAMP" &>>$LOG_FILE_NAME
