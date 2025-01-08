496bf0654b3609a3979500634b56bd78f24f0772
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-08 09:56:22 +0530

    backup logs

---
+++ 18-backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not given days count, you can mention 14days by default
+
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+   then 
+      echo -e "$2....$R failure $N"
+      exit 1
+    else 
+      echo -e "$2... $G success $N"
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+}
+
+if[ $# -lt 2 ]
+then 
+  echo USAGE
+fi
+
+echo "script started exceuting.. $TIMESTAMP" &>>$LOG_FILE_NAME
+
