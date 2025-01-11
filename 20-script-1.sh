dcb8194352a0f67aef966b11c98a6d44a966b7d4
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-11 22:52:18 +0530

    script 1 and script2

--- 20-script-1.sh
+++ 20-script-1.sh
@@ -1,12 +1,16 @@
+
 #!/bin/bash
 
 MSG="message from script1"
-GREET="greetings from script1"
+export GREET="greetings from script1"
 
 echo $MSG
 
 # Export the GREET variable so it is available to script2
-export GREET
+
 
 # Call script2
 bash 21-script-2.sh
+
+
+
