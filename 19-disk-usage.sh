1687d96d4f74214975891c90eaba448916e7a0ee
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-11 17:05:44 +0530

    disk usage added

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-#DISK_THRESHOLD=5 #real project 80
-#USAGE=$(echo $line | awk -F " " '{print $6F}')  this line replace 8line
+DISK_THRESHOLD=5 #real project 80
+
 #PARTITION=$(echo $line | awk -F " " '{print $NF}') add line9
 #echo "partition: $PARTITION, Usage: $USAGE" add line10
 
 while read -r line
 do
- echo $line
+ USAGE=$(echo $line | awk -F " " '{print $6F}') 
 done <<< $DISK_USAGE
