f4ea005a7d771691d2c39b7f133a03a90c8f62db
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-11 21:42:33 +0530

    Disk usage updated

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -11,11 +11,19 @@ do
  #echo "partition: $PARTITION, Usage: $USAGE"
  if [ $USAGE -ge $DISK_THRESHOLD ]
  then
-   MSG+=" High disk usage on partition: $PARTITION usage is : $USAGE \n " #+= it will not replace any message give output entire
+   MSG+=" High disk usage on partition: $PARTITION usage is : $USAGE\n " #+= it will not replace any message give output entire
  fi
 
 done <<< $DISK_USAGE
 
-echo -e "Message: $MSG"
+# echo -e "Message: $MSG"
 
-echo "$MSG" | mutt -s "High Disk Usage" kumarreddyj007@gmail.com
+# echo "$MSG" | mutt -s "High Disk Usage" kumarreddyj007@gmail.com
+
+# Replace \n with actual newlines for clean output
+FORMATTED_MSG=$(echo -e "$MSG")
+
+echo -e "Message:\n$FORMATTED_MSG"
+
+# Sending email
+echo -e "$FORMATTED_MSG" | mutt -s "High Disk Usage" kumarreddyj007@gmail.com
