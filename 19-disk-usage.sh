fe645228e9f5880b8850ba1f3b035481638260fe
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-11 21:30:27 +0530

    Disk usage updated

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -7,6 +7,10 @@ while read -r line
 do
  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1) 
  PARTITION=$(echo $line | awk -F " " '{print $NF}') 
- echo "partition: $PARTITION, Usage: $USAGE"
+ #echo "partition: $PARTITION, Usage: $USAGE"
+ if [ $USAGE -ge $DISK_THRESHOLD ]
+ then
+   MSG=" High disk usage on partition: $PARTITION usage is : $USAGE"
+ fi
 
 done <<< $DISK_USAGE
