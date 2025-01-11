4ce313297e115e9ac4a39f97cf948045653a94dd
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-01-11 22:46:15 +0530

    script 1 and script2

--- 20-script-1.sh
+++ 20-script-1.sh
@@ -5,8 +5,8 @@ GREET="greetings from script1"
 
 echo $MSG
 
-# Export the GREET variable so it can be accessed in script2
+# Export the GREET variable so it is available to script2
 export GREET
 
 # Call script2
-sh 21-script-2.sh
+bash 21-script-2.sh
--- 21-script-2.sh
+++ 21-script-2.sh
@@ -3,4 +3,4 @@
 MSG="message from script2"
 
 echo $MSG
-echo $GREET # Access the GREET variable exported from script1
+echo "GREET variable in script2: $GREET"
